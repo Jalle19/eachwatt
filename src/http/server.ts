05d027c9a66b1b6b04c41c9617c6a56918313c45
Sam Stenvall <sam.stenvall@nitor.com>
2023-09-22 15:19:01 +0300

    Add initial real-time web interface

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,16 +1,27 @@
 import { IncomingMessage, RequestListener, ServerResponse } from 'http'
+import fs from 'fs'
 import { promises as fsPromisified } from 'fs'
 
 export const httpRequestHandler: RequestListener = async (req: IncomingMessage, res: ServerResponse) => {
   // Main routing logic
-  switch (req.url) {
+  let reqUrl = req.url
+  if (reqUrl?.indexOf('?') !== -1) {
+    reqUrl = reqUrl?.substring(0, reqUrl?.indexOf('?'))
+  }
+
+  switch (reqUrl) {
     case '/':
     case '/index.html':
-      const indexContents = await fsPromisified.readFile(resolveFilePath('index.html'))
-
-      res.setHeader('Content-Type', 'text/html')
-      res.writeHead(200)
-      res.end(indexContents)
+      await serveStaticFile('index.html', 'text/html', res)
+      break
+    case '/eachwatt.css':
+      await serveStaticFile('eachwatt.css', 'text/css', res)
+      break
+    case '/eachwatt.js':
+      await serveStaticFile('eachwatt.js', 'text/javascript', res)
+      break
+    case '/eachwatt_logo.png':
+      await serveStaticFile('eachwatt_logo.png', 'image/png', res)
       break
     default:
       res.writeHead(404)
@@ -21,4 +32,12 @@ export const httpRequestHandler: RequestListener = async (req: IncomingMessage, 
 
 const resolveFilePath = (file: string): string => {
   return __dirname + '/../../webif/' + file
+}
+
+const serveStaticFile = async (file: string, contentType: string, res: ServerResponse): Promise<void> => {
+  const fileContents = await fsPromisified.readFile(resolveFilePath(file))
+
+  res.setHeader('Content-Type', contentType)
+  res.writeHead(200)
+  res.end(fileContents)
 }
