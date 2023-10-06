2eb6daf1237a78d778c51183c0113ccb5aa18957
Sam Stenvall <sam.stenvall@nitor.com>
2023-10-06 22:29:18 +0300

    Improve the built-in HTTP server so it can serve the Svelte app

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,42 +1,63 @@
+import fs from 'fs'
+import path from 'path'
 import { IncomingMessage, RequestListener, ServerResponse } from 'http'
 import { promises as fsPromisified } from 'fs'
 
+const basePath = __dirname + '/../../webif/build'
+
+const mimeTypes = new Map<string, string>([
+  ['.html', 'text/html'],
+  ['.js', 'text/javascript'],
+  ['.css', 'text/css'],
+  ['.json', 'application/json'],
+  ['.png', 'image/png'],
+])
+
 export const httpRequestHandler: RequestListener = async (req: IncomingMessage, res: ServerResponse) => {
-  // Main routing logic
-  let reqUrl = req.url
+  const filePath = resolveFilePath(req.url)
+
+  // Serve 404 if file doesn't exist
+  if (!fs.existsSync(filePath)) {
+    res.writeHead(404)
+    res.end('Not found')
+    return
+  }
+
+  // File exists, try to determine MIME type
+  const extension = path.extname(filePath).toLowerCase()
+  const mimeType = mimeTypes.get(extension)
+
+  await serveStaticFile(filePath, mimeType, res)
+}
+
+const resolveFilePath = (reqUrl: string | undefined): string => {
+  // Strip query parameters
   if (reqUrl?.indexOf('?') !== -1) {
     reqUrl = reqUrl?.substring(0, reqUrl?.indexOf('?'))
   }
 
-  switch (reqUrl) {
-    case '/':
-    case '/index.html':
-      await serveStaticFile('index.html', 'text/html', res)
-      break
-    case '/eachwatt.css':
-      await serveStaticFile('eachwatt.css', 'text/css', res)
-      break
-    case '/eachwatt.js':
-      await serveStaticFile('eachwatt.js', 'text/javascript', res)
-      break
-    case '/eachwatt_logo.png':
-      await serveStaticFile('eachwatt_logo.png', 'image/png', res)
-      break
-    default:
-      res.writeHead(404)
-      res.end('Not found')
-      break
+  // "Convert" to file path
+  let filePath
+  if (!reqUrl || reqUrl === '/') {
+    filePath = '/index.html'
+  } else {
+    filePath = reqUrl
   }
+
+  return basePath + filePath
 }
 
-const resolveFilePath = (file: string): string => {
-  return __dirname + '/../../webif/' + file
-}
+const serveStaticFile = async (
+  filePath: string,
+  contentType: string | undefined,
+  res: ServerResponse,
+): Promise<void> => {
+  const fileContents = await fsPromisified.readFile(filePath)
 
-const serveStaticFile = async (file: string, contentType: string, res: ServerResponse): Promise<void> => {
-  const fileContents = await fsPromisified.readFile(resolveFilePath(file))
+  if (contentType !== undefined) {
+    res.setHeader('Content-Type', contentType)
+  }
 
-  res.setHeader('Content-Type', contentType)
   res.writeHead(200)
   res.end(fileContents)
 }
