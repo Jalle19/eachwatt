1fa96e6b953f944737a63ad22665cf8cc13a4923
Sam Stenvall <sam.stenvall@nitor.com>
2023-10-13 22:34:56 +0300

    Add <thead> and <tbody>

--- webif/src/routes/Characteristics.svelte
+++ webif/src/routes/Characteristics.svelte
@@ -4,18 +4,22 @@
 
 <h2>Characteristics</h2>
 <table>
-  <tr>
-    <th>Name</th>
-    <th>Phase</th>
-    <th class="cell-right-align">Voltage</th>
-    <th class="cell-right-align">Frequency</th>
-  </tr>
-  {#each sensorData as data}
+  <thead>
     <tr>
-      <td>{data.characteristics.name}</td>
-      <td>{data.characteristics.phase}</td>
-      <td class="cell-right-align">{data.voltage}V</td>
-      <td class="cell-right-align">{data.frequency}Hz</td>
+      <th>Name</th>
+      <th>Phase</th>
+      <th class="cell-right-align">Voltage</th>
+      <th class="cell-right-align">Frequency</th>
     </tr>
-  {/each}
+  </thead>
+  <tbody>
+    {#each sensorData as data}
+      <tr>
+        <td>{data.characteristics.name}</td>
+        <td>{data.characteristics.phase}</td>
+        <td class="cell-right-align">{data.voltage}V</td>
+        <td class="cell-right-align">{data.frequency}Hz</td>
+      </tr>
+    {/each}
+  </tbody>
 </table>
--- webif/src/routes/Circuits.svelte
+++ webif/src/routes/Circuits.svelte
@@ -4,20 +4,24 @@
 
 <h2>All circuits</h2>
 <table>
-  <tr>
-    <th>Circuit</th>
-    <th>Group</th>
-    <th>Circuit type</th>
-    <th>Sensor type</th>
-    <th class="cell-right-align">Power</th>
-  </tr>
-  {#each sensorData as data}
+  <thead>
     <tr>
-      <td>{data.circuit.name}</td>
-      <td>{data.circuit.group ?? ''}</td>
-      <td>{data.circuit.type}</td>
-      <td>{data.circuit.sensor.type}</td>
-      <td class="cell-right-align">{data.watts}W</td>
+      <th>Circuit</th>
+      <th>Group</th>
+      <th>Circuit type</th>
+      <th>Sensor type</th>
+      <th class="cell-right-align">Power</th>
     </tr>
-  {/each}
+  </thead>
+  <tbody>
+    {#each sensorData as data}
+      <tr>
+        <td>{data.circuit.name}</td>
+        <td>{data.circuit.group ?? ''}</td>
+        <td>{data.circuit.type}</td>
+        <td>{data.circuit.sensor.type}</td>
+        <td class="cell-right-align">{data.watts}W</td>
+      </tr>
+    {/each}
+  </tbody>
 </table>
