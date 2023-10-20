c2602ef60dba1ceff5671d0541d8cfddc009ee81
Sam Stenvall <sam.stenvall@nitor.com>
2023-10-20 09:43:52 +0300

    Add unit tests for topic naming functions, switch to lower-case

--- src/publisher/mqtt/util.ts
+++ src/publisher/mqtt/util.ts
@@ -4,8 +4,12 @@ import { TOPIC_PREFIX } from '../mqtt'
 import slugify from 'slugify'
 
 const slugifyName = (name: string): string => {
-  // We can't have "+" in MQTT topic names
-  return slugify(name, { remove: /[+]/ })
+  return slugify(name, {
+    // We can't have "+" in MQTT topic names
+    remove: /[+]/,
+    // Since the rest of the topic name is lower-case, just lower-case everything
+    lower: true,
+  })
 }
 
 export const createCharacteristicsSensorTopicName = (characteristics: Characteristics, value: string): string => {
