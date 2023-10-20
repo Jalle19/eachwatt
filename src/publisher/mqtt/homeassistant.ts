8adf23721c92e1fba8e1de418ea8d8aeb77cbda9
Sam Stenvall <sam.stenvall@nitor.com>
2023-10-20 20:22:44 +0300

    Refactor Home Assistant configuration publishing to be more modular

--- src/publisher/mqtt/homeassistant.ts
+++ src/publisher/mqtt/homeassistant.ts
@@ -3,90 +3,95 @@ import { Config } from '../../config'
 import { TOPIC_NAME_STATUS } from '../mqtt'
 import { createPowerSensorTopicName, slugifyName } from './util'
 import { supportsApparentPower, supportsPowerFactor } from '../../sensor'
+import { Circuit } from '../../circuit'
 
 export const configureMqttDiscovery = async (config: Config, mqttClient: MqttClient): Promise<void> => {
-  // The "device" object that is part of each sensor's configuration payload
-  const mqttDeviceInformation = {
-    'name': 'eachwatt',
-    'model': 'Eachwatt',
-    'identifiers': 'eachwatt',
-  }
-
-  const configurationBase = {
-    'platform': 'mqtt',
-    'availability_topic': TOPIC_NAME_STATUS,
-    'device': mqttDeviceInformation,
-    'state_class': 'measurement',
-  }
-
   const promises = []
 
   for (const circuit of config.circuits) {
-    const entityName = slugifyName(circuit.name)
-
     // Add power sensors
-    const uniqueId = `eachwatt_${entityName}_power`
-    const configurationTopicName = `homeassistant/sensor/${uniqueId}/config`
-    const configuration = {
-      ...configurationBase,
-      'device_class': 'power',
-      'unit_of_measurement': 'W',
-      'name': `${circuit.name} power`,
-      'unique_id': uniqueId,
-      'object_id': uniqueId,
-      'state_topic': createPowerSensorTopicName(circuit, 'power'),
-    }
-
-    // "retain" is used so that the entities will be available immediately after a Home Assistant restart
-    promises.push(
-      mqttClient.publishAsync(configurationTopicName, JSON.stringify(configuration), {
-        retain: true,
-      }),
-    )
+    promises.push(publishPowerSensorConfiguration(mqttClient, circuit))
 
     // Add apparent power sensors
     if (supportsApparentPower(circuit.sensor)) {
-      const uniqueId = `eachwatt_${entityName}_apparentPower`
-      const configurationTopicName = `homeassistant/sensor/${uniqueId}/config`
-      const configuration = {
-        ...configurationBase,
-        'device_class': 'apparent_power',
-        'unit_of_measurement': 'VA',
-        'name': `${circuit.name} apparent power`,
-        'unique_id': uniqueId,
-        'object_id': uniqueId,
-        'state_topic': createPowerSensorTopicName(circuit, 'apparentPower'),
-      }
-
-      // "retain" is used so that the entities will be available immediately after a Home Assistant restart
-      promises.push(
-        mqttClient.publishAsync(configurationTopicName, JSON.stringify(configuration), {
-          retain: true,
-        }),
-      )
+      promises.push(publishApparentPowerSensorConfiguration(mqttClient, circuit))
     }
 
     // Add power factor sensors
     if (supportsPowerFactor(circuit.sensor)) {
-      const uniqueId = `eachwatt_${entityName}_powerFactor`
-      const configurationTopicName = `homeassistant/sensor/${uniqueId}/config`
-      const configuration = {
-        ...configurationBase,
-        'device_class': 'power_factor',
-        'name': `${circuit.name} power factor`,
-        'unique_id': uniqueId,
-        'object_id': uniqueId,
-        'state_topic': createPowerSensorTopicName(circuit, 'powerFactor'),
-      }
-
-      // "retain" is used so that the entities will be available immediately after a Home Assistant restart
-      promises.push(
-        mqttClient.publishAsync(configurationTopicName, JSON.stringify(configuration), {
-          retain: true,
-        }),
-      )
+      promises.push(publishPowerFactorSensorConfiguration(mqttClient, circuit))
     }
   }
 
   await Promise.all(promises)
+}
+
+const getConfigurationBase = (): object => {
+  return {
+    'platform': 'mqtt',
+    'availability_topic': TOPIC_NAME_STATUS,
+    'state_class': 'measurement',
+    'device': {
+      'name': 'eachwatt',
+      'model': 'Eachwatt',
+      'identifiers': 'eachwatt',
+    },
+  }
+}
+
+const publishPowerSensorConfiguration = async (mqttClient: MqttClient, circuit: Circuit) => {
+  const entityName = slugifyName(circuit.name)
+  const uniqueId = `eachwatt_${entityName}_power`
+  const configurationTopicName = `homeassistant/sensor/${uniqueId}/config`
+  const configuration = {
+    ...getConfigurationBase(),
+    'device_class': 'power',
+    'unit_of_measurement': 'W',
+    'name': `${circuit.name} power`,
+    'unique_id': uniqueId,
+    'object_id': uniqueId,
+    'state_topic': createPowerSensorTopicName(circuit, 'power'),
+  }
+
+  return mqttClient.publishAsync(configurationTopicName, JSON.stringify(configuration), {
+    retain: true,
+  })
+}
+
+const publishApparentPowerSensorConfiguration = async (mqttClient: MqttClient, circuit: Circuit) => {
+  const entityName = slugifyName(circuit.name)
+  const uniqueId = `eachwatt_${entityName}_apparentPower`
+  const configurationTopicName = `homeassistant/sensor/${uniqueId}/config`
+  const configuration = {
+    ...getConfigurationBase(),
+    'device_class': 'apparent_power',
+    'unit_of_measurement': 'VA',
+    'name': `${circuit.name} apparent power`,
+    'unique_id': uniqueId,
+    'object_id': uniqueId,
+    'state_topic': createPowerSensorTopicName(circuit, 'apparentPower'),
+  }
+
+  return mqttClient.publishAsync(configurationTopicName, JSON.stringify(configuration), {
+    retain: true,
+  })
+}
+
+const publishPowerFactorSensorConfiguration = async (mqttClient: MqttClient, circuit: Circuit) => {
+  const entityName = slugifyName(circuit.name)
+  const uniqueId = `eachwatt_${entityName}_powerFactor`
+  const configurationTopicName = `homeassistant/sensor/${uniqueId}/config`
+  const configuration = {
+    ...getConfigurationBase(),
+    'device_class': 'power_factor',
+    'name': `${circuit.name} power factor`,
+    'unique_id': uniqueId,
+    'object_id': uniqueId,
+    'state_topic': createPowerSensorTopicName(circuit, 'powerFactor'),
+  }
+
+  // "retain" is used so that the entities will be available immediately after a Home Assistant restart
+  return mqttClient.publishAsync(configurationTopicName, JSON.stringify(configuration), {
+    retain: true,
+  })
 }
