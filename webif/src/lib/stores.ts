25f727ae4b06f67cc7329953d0ca3a6a1a049fd3
Sam Stenvall <sam.stenvall@nitor.com>
2023-10-31 10:14:44 +0200

    Fix some IDE errors by defining types for store contents

--- webif/src/lib/stores.ts
+++ webif/src/lib/stores.ts
@@ -1,8 +1,11 @@
 import { writable } from 'svelte/store'
 
+type LastUpdateTimestamp = Date | undefined
+type WebSocketUrl = string | URL | undefined
+
 export const configurationStore = writable({})
 export const characteristicsStore = writable([])
 export const mainSensorDataStore = writable([])
 export const circuitSensorDataStore = writable([])
-export const lastUpdateTimestampStore = writable(undefined)
-export const webSocketUrlStore = writable(undefined)
+export const lastUpdateTimestampStore = writable(undefined as LastUpdateTimestamp)
+export const webSocketUrlStore = writable(undefined as WebSocketUrl)
