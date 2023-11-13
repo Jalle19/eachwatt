43b7fd97f0fa1df4de2fe120eb096a804c01cd6c
Sam Stenvall <sam.stenvall@nitor.com>
2023-11-13 09:56:36 +0200

    Move CSS to separate file

--- webif/src/routes/+layout.svelte
+++ webif/src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
     import 'purecss/build/pure.css'
+    import './styles.css'
 
     import {
       configurationStore,
@@ -82,86 +83,5 @@
 </div>
 
 <style>
-    :root {
-        --background: #fff;
-        --color: #222;
-        --component-background: #e0e0e0;
-        --highlight-color: #ff3d00;
-    }
 
-    @media (prefers-color-scheme: dark){
-        :root {
-            --background: #272727;
-            --color: #aaa;
-            --component-background: #3b3b3b;
-            --highlight-color: #ff3d00;
-        }
-
-        nav {
-            background-color: var(--component-background);
-        }
-
-        /* pure-table overrides */
-        :global(.pure-table) {
-            border: 2px solid #424242;
-        }
-        :global(.pure-table thead) {
-            background-color: var(--component-background);
-            color: var(--color);
-        }
-
-        :global(.pure-table th, .pure-table td) {
-            border: 0;
-        }
-
-        :global(.pure-table-striped tr:nth-child(2n-1) td) {
-            background-color: #2E2E2E;
-        }
-        /* end pure-table overrides */
-    }
-
-    :global(body) {
-        background: var(--background);
-        color: var(--color);
-    }
-
-    :global(h1, h2, h3, h4, h5, h6) {
-        margin-top: 0;
-        padding-bottom: 0.2em;
-        display: inline-block;
-        border-bottom: 2px solid var(--highlight-color);
-    }
-
-    .container {
-        max-width: 1200px;
-        margin: 0 auto;
-        clear: both;
-    }
-
-    :global(table) {
-        width: 100%;
-    }
-
-    :global(.cell-right-align) {
-        text-align: right;
-    }
-
-    :global(.pure-g > div) {
-        box-sizing: border-box;
-    }
-
-    :global(.l-box) {
-        padding: 1em;
-    }
-
-    /* pure-menu tweaks */
-    :global(.pure-menu-link) {
-      border-bottom: 2px solid var(--component-background);
-    }
-
-    :global(.pure-menu-link:hover) {
-        border-bottom: 2px solid var(--highlight-color);
-        background: inherit !important;
-    }
-    /* end pure-menu tweaks */
 </style>
