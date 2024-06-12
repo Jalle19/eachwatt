139d540028147c2ad8e594695d248b1738fa3b61
Sam Stenvall <sam.stenvall@nitor.com>
2024-06-12 12:39:07 +0300

    Remove unused parameter

--- src/sensor/dummy.ts
+++ src/sensor/dummy.ts
@@ -12,8 +12,6 @@ import { Characteristics } from '../characteristics'
 export const getSensorData: PowerSensorPollFunction = async (
   timestamp: number,
   circuit: Circuit,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  existingSensorData?: PowerSensorData[],
 ): Promise<PowerSensorData> => {
   return emptySensorData(timestamp, circuit)
 }
